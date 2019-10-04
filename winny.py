43505d453b7b63c556377186333d598a58e9f1f4
Winny <winston@ml1.net>
2019-10-03 19:20:32 -0500

    Initialize addfactorial

---
+++ winny.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def fact(n):
+    m = 1
+    for i in range(1, n+1):
+        m *= i
+    return m
+
+if __name__ == '__main__':
+    number = int(input('Input a number: '))
+    print(fact(number))
